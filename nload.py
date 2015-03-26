16c7d70cecf6b6d14dee7b907aa61b10c48d81c6
Albert DeFusco <defusco@pitt.edu>
2015-03-26 11:07:29 -0400

    collect multiple interfaces
    lo is ignored
    
    TODO: subplots

--- nload.py
+++ nload.py
@@ -26,43 +26,47 @@ if __name__ == '__main__':
 
 
   load = {}
+  prev = {}
+  instant = {}
   for net in get_interfaces():
     load[net] = list()
-    prev[net] = (0,0)
-
-  rx_prev=0
-  tx_prev=0
+    prev[net] = [0,0] #rx,tx
+    instant[net] = [0,0]
 
   print "Collecting network traffic."
-  print "Interfaces :" + get_interfaces()
+  nets=''.join([("%s "%(net)) for net in get_interfaces()])
+  print "Interfaces: " + nets
   print "Press ^C to terminate and generate plots"
   while(True):
     try:
       now=time.time()
-      tx = get_bytes('tx')
-      rx = get_bytes('rx')
+      for net in get_interfaces():
+        instant[net][0] = get_bytes('rx',iface=net)
+        instant[net][1] = get_bytes('tx',iface=net)
 
-      if rx_prev>0 and tx_prev>0:
-        rx_speed=rx-rx_prev
-        tx_speed=tx-tx_prev
-	load.append( (now-start,rx_speed,tx_speed) )
+        if prev[net][0]>0 and prev[net][1]>0:
+	  rx_speed=instant[net][0]-prev[net][0]
+	  tx_speed=instant[net][1]-prev[net][1]
+	  load[net].append( (now-start,rx_speed,tx_speed) )
         #print "%10d  %10d" % (rx_speed,tx_speed)
 
       time.sleep(1)
 
-      tx_prev = tx
-      rx_prev = rx
+      for net in get_interfaces():
+        prev[net][0] = instant[net][0]
+	prev[net][1] = instant[net][1]
+
     except KeyboardInterrupt:
       print
       print "Make graph"
       host=socket.gethostname()
-      x=[l[0]/60. for l in load]
-      t=[l[1]/1024./1024. for l in load]
-      r=[l[2]/1024./1024. for l in load]
-      pyplot.plot(x,t,'r-',label='recieve')
-      pyplot.plot(x,r,'b-',label='send')
+      x=[l[0]/60. for l in load['eth1']]
+      t=[l[1]/1024./1024. for l in load['eth1']]
+      r=[l[2]/1024./1024. for l in load['eth1']]
+      pyplot.plot(x,t,'b-',label='incoming')
+      pyplot.plot(x,r,'r-',label='outgoing')
       pyplot.legend()
-      pyplot.title(host+' eth0\nStart Time: '+date)
+      pyplot.title(host+' eth1\nStart Time: '+date)
       pyplot.xlabel('Minutes')
       pyplot.ylabel('Network traffic (MB)')
       pyplot.savefig( 'eth0.png' )
